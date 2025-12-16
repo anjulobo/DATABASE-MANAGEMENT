545d671873c96daa49285c61865f3021115c6e90
anjulobo <anjusingh72@gmail.com>
2025-12-16 10:07:26 +0530

    Delete Sql quary on comparison operators.sql

--- Sql quary on comparison operators.sql
+++
@@ -1,8 +0,0 @@
-# comparison operators
-select*from student2 where age>18;
-select*from student2 where age<18;
-select * from student2 where age<20;
-select * from student2 where age>=18;
-select * from student2 where age<=20;
-select * from student2 where city="agra";
-select * from student2 where city<>"agra";
